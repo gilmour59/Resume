5deecdbb87a4dab469a4ef4e8dd5b812e18837bb
gilmour <gilmouralmalbisdev@gmail.com>
2024-01-30 09:13:20 +0800

    first commit

---
+++ app/Controllers/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\UserModel;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class UserController extends BaseController
+{
+    public function index()
+    {
+        return view('errors/html/production');
+    }
+
+    public function create()
+    {
+    }
+
+    public function store()
+    {
+    }
+
+    public function edit()
+    {
+    }
+
+    
+}
